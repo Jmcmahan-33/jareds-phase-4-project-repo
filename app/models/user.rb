4ec681ad83d32ef1601526eae8cd79caca756257
Jared McMahan <jaredm788@gmail.com>
2023-04-06 12:04:15 -0600

    fixing associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
     has_many :appointments
-    has_many :users, through: :appointments
+    has_many :doctors, through: :appointments
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,7 @@
-Doctor.create(name: "Doctor Octavis", speciality: "brain surgeon", session_length: 1, rate: 60, notes: "In on Tuesdays")
+# doc1= Doctor.create(name: "Doctor Octavis", speciality: "brain surgeon", room_number: "1b", rate: 60, notes: "In on Tuesdays")
+
+# user1= User.create(full_name: "Rachel Adams", age: 30,username: "Radams@1234", password_digest: "testingRachel")
+
+# apt1= Appointment.create(doctor_id: doc1.id, user_id: user2.id, reason_for_visit: "really bad headaches")
+
+
