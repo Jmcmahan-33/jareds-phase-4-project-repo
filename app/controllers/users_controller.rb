9aadd115bc79d1cd79c3faf5bca9c7e0bd25fbc2
Jared McMahan <jaredm788@gmail.com>
2023-04-06 13:38:59 -0600

    creating user for sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,23 @@
 class UsersController < ApplicationController
+    skip_before_action :authorize, only: :create
+    skip_before_action :authorize, only: :index
+
+    def index
+        users = User.all
+        render json: users
+    end 
+    def create
+        # create the user(signup)
+        user = User.create!(user_params)
+        # save the newly created id to a session hash
+        session[:user_id] = user.id 
+        # return json 
+        render json: user, status: :created
+
+    end
+
+    private
+    def user_params
+        params.permit(:full_name, :age, :username, :password, :password_confirmation) 
+    end
 end
