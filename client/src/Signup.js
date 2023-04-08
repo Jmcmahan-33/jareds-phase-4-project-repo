4a78359f576541a748c172e37f6cdd4d85fa3960
Jared McMahan <jaredm788@gmail.com>
2023-04-08 09:56:55 -0600

    Signup form starting

---
+++ client/src/Signup.js
@@ -0,0 +1,23 @@
+function Signup() {
+
+    return (
+        <div>
+            <form>
+                <label>Patient Name:</label>
+                <input 
+                type="text"
+                /> <br/>
+                <label>Username: </label>
+                <input
+                type="text"
+                id="username"
+                
+                /> <br/>
+                <button>BUTTON</button>
+            </form>
+
+        </div>
+    )
+}
+
+export default Signup
