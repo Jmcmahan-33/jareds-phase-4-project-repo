408e7e5c04636be09baa8e85aad16c7ebe9fdf23
Jared McMahan <jaredm788@gmail.com>
2023-04-11 11:36:25 -0600

    Render testing

--- client/src/Home.js
+++ client/src/Home.js
@@ -3,13 +3,32 @@
 import React, { useContext } from "react";
 import { UserContext } from "./context/user";
 
+// function Home() {
+//     const { user, loggedIn } = useContext(UserContext)
+//     if (loggedIn) {
+//         return (
+
+//             <div>
+//                 <h1>{user.username} Homepage</h1>
+//             </div>
+//         )
+//     } else {
+//         return (
+//             <div>
+//                 <h1>MyHealth</h1>
+//                 <h2>Sign up or Log In</h2>
+//             </div>
+
+//         )
+//     }
+// }
 function Home() {
     const { user, loggedIn } = useContext(UserContext)
-    if (loggedIn) {
+    if (!user) {
         return (
 
             <div>
-                <h1>{user.username}</h1>
+                <h1>{user.username} Homepage</h1>
             </div>
         )
     } else {
@@ -22,5 +41,6 @@ function Home() {
         )
     }
 }
+
 
 export default Home
