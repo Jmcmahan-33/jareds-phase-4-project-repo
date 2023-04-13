2d5b206f889675bee4f362a912371dcd0d608124
Jared McMahan <jaredm788@gmail.com>
2023-04-13 14:35:33 -0600

    Doctors feature app breaking

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,8 +1,45 @@
 class DoctorsController < ApplicationController
-    skip_before_action :authorize, only: :index
+    skip_before_action :authorize
 
     def index
-        doctors = Doctor.all
+        # index doctors for only the current user. 
+        doctors = current_user.doctors
         render json: doctors
     end 
+
+    def create
+        doctor = current_user.doctors.create(doctor_params)
+        if doctor.valid? 
+            render json: doctor 
+        else
+            render json: {errors: doctors.errors.full_messages}, status: :unprocessable_entity
+        end
+    end
+
+    def show
+        doctor = current_user.doctors.find_by(id: params[:id])
+        if doctor 
+            render json: doctor
+        else
+            render json: {errors: "Not Found"}, status: :unauthorized
+        end
+    end 
+
+    private
+
+    def current_user
+        User.find_by(id: session[:user_id])
+    end
+
+
+    def doctor_params
+        params.permit(:name, :speciality, :room_number, :rate, :notes)
+    end 
+
+    def authorize
+        return render json: {error: "Not Authorized"}, status: :unauthorized unless session.include? :user_id
+    end 
+
+
+
 end
--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,4 +1,5 @@
 class Doctor < ApplicationRecord
     has_many :appointments
     has_many :users, through: :appointments
+    validates :name true, uniqueness: true
 end
