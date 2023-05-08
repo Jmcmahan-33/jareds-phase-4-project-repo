6422426440fed22bb20a256274c3d1e7f64fb62e
Jared McMahan <jaredm788@gmail.com>
2023-05-07 20:45:27 -0600

    route cleanup

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -2,7 +2,6 @@ class Appointment < ApplicationRecord
   belongs_to :doctor
   belongs_to :user
 
-  # here lies the issue of creating a doctor. Its trying to validate reason for visit because appointments is connected to doctors.
 
   validates :reason_for_visit, presence: true 
   validates :date_field, presence: true
--- config/routes.rb
+++ config/routes.rb
@@ -4,16 +4,11 @@ Rails.application.routes.draw do
   resources :users
   resources :doctors
 
-  # appointments routes
-  get "/appointments", to: "appointments#index"
-  patch "/appointment/:id", to: "appointments#update"
-  # users routes
   post "/signup", to: "users#create"
   get "/me", to: "users#show"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
-  # doctors routes
-  post "/doctors", to: "doctors#create"
+
   
   # get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
